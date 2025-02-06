e923463b6662f8764f0ac8da80d4d86143b2d0ed
Khaled-Gasmi-28 <khaled.gasmi.9000@gmail.com>
2025-02-06 19:59:10 +0100

    fist build

---
+++ ProjetCpp/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ProjetCpp_FILE E:/QT-Projects/Projet Cpp/ProjetCpp/ProjetCpp/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/ProjetCpp.exe)
+set(__QT_DEPLOY_TARGET_ProjetCpp_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ProjetCpp_RUNTIME_DLLS E:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
